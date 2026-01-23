97a1b7de30e386735a6b24c29b1207db347fd361
Logan Richardson <logabrichardson13@gmail.com>
2026-01-22 18:15:41 -0700

    trying to reconfigure files so allow for movesCalculator

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a single chess piece
@@ -14,6 +15,10 @@ public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
 
+    private static final Map<PieceType, MovesCalculator> calculator = Map.of(
+            PieceType.BISHOP, new BishopMovesCalculator()
+
+    );
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
@@ -55,5 +60,39 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         MovesCalculator piece = board.getPiece(myPosition);
         return List.of();
+    }
+}
+
+private boolean inBounds(int r, int c) {
+    return (r >= 1) && (r <= 8) && (c >= 1) && (c <= 8);
+}
+
+public interface MovesCalculator {
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
+}
+
+public class BishopMovesCalculator implements MovesCalculator {
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        ChessPiece bishop = board.getPiece(position);
+        if (bishop == null) {
+            return List.of();
+        }
+        var moves = new ArrayList<ChessMove>();
+        int[][] directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
+        for (int[] d : directions) {
+            int r = position.getRow() + d[0];
+            int c = position.getColumn() + d[1];
+            while (inBounds(r, c)) {
+                ChessPosition end = new ChessPosition(r,c);
+                ChessPiece space = board.getPiece(end);
+                if (space == null) {
+                    moves.add(new ChessMove(position, end, null));
+                }
+                r += d[0];
+                c += d[1];
+            }
+        }
+        return moves;
     }
 }
