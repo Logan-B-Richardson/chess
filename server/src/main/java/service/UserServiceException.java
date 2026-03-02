48e0b9f766ab69dbdb3c369f976288ed7bfc2980
Logan Richardson <logabrichardson13@gmail.com>
2026-03-02 15:13:58 -0700

    ForbiddenExcpetion

--- server/src/main/java/service/UserServiceException.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public class UserServiceException extends Exception{
-    public ForbiddenException(String message) {super(message);}
-}
---
+++ server/src/main/java/service/exception/ForbiddenException.java
@@ -0,0 +1,5 @@
+package service.exception;
+
+public class ForbiddenException extends Exception {
+    public ForbiddenException(String message) {super(message);}
+}
