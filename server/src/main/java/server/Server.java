52f492784ac0b26d8535c4dff6172ac7948a0cec
Logan Richardson <logabrichardson13@gmail.com>
2026-03-02 20:01:40 -0700

    setup server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,6 +3,8 @@ package server;
 import io.javalin.*;
 import com.google.gson.Gson;
 import dataaccess.*;
+import handler.*;
+import service.*;
 
 public class Server {
 
@@ -11,6 +13,17 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        DataAccess dao = new DataAccessMemory();
+        UserService userService = new UserService(dao);
+        GameService gameService = new GameService(dao);
+        ClearService clearService = new ClearService(dao);
+        RegisterHandler registerHandler = new RegisterHandler(userService, gson);
+        LoginHandler loginHandler = new LoginHandler(userService, gson);
+        LogoutHandler logoutHandler = new LogoutHandler(userService);
+        CreateGameHandler createGameHandler = new CreateGameHandler(gameService, gson);
+        JoinGameHandler joinGameHandler = new JoinGameHandler(gameService, gson);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
     }
 
     public int run(int desiredPort) {
