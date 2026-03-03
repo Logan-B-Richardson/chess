4c78a33d040ec8852fbda9693da8a8f0547a65c9
Logan Richardson <logabrichardson13@gmail.com>
2026-03-02 20:06:15 -0700

    url routes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -13,6 +13,8 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        // handler and service setup
         DataAccess dao = new DataAccessMemory();
         UserService userService = new UserService(dao);
         GameService gameService = new GameService(dao);
@@ -20,10 +22,19 @@ public class Server {
         RegisterHandler registerHandler = new RegisterHandler(userService, gson);
         LoginHandler loginHandler = new LoginHandler(userService, gson);
         LogoutHandler logoutHandler = new LogoutHandler(userService);
+        ListGamesHandler listGamesHandler = new ListGamesHandler(gameService, gson);
         CreateGameHandler createGameHandler = new CreateGameHandler(gameService, gson);
         JoinGameHandler joinGameHandler = new JoinGameHandler(gameService, gson);
         ClearHandler clearHandler = new ClearHandler(clearService);
 
+        // url routes
+        javalin.delete("/db", clearHandler::handle);
+        javalin.post("/user", registerHandler::handle);
+        javalin.post("/session", loginHandler::handle);
+        javalin.delete("/session", logoutHandler::handle);
+        javalin.get("/game", listGamesHandler::handle);
+        javalin.post("/game", createGameHandler::handle);
+        javalin.put("/game", joinGameHandler::handle);
     }
 
     public int run(int desiredPort) {
