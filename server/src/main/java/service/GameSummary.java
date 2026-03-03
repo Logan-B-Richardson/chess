1f729a06e8f8c897a9128d7684690ec547379111
Logan Richardson <logabrichardson13@gmail.com>
2026-03-02 18:18:54 -0700

    correction

--- server/src/main/java/service/GameSummary.java
+++ server/src/main/java/service/GameSummary.java
@@ -1,4 +1,4 @@
 package service;
 
-public record GameSummary() {
+public record GameSummary(int gameid, String whitename, String blackname, String gamename) {
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,4 @@
 package service;
 
-public record UserService(int gameid, String whitename, String blackname, String gamename) {
+public class UserService {
 }
