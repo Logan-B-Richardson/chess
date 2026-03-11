575d388f1067121d6b48fbb992295e988abbfa56
Logan Richardson <logabrichardson13@gmail.com>
2026-03-10 20:00:02 -0600

    get user imp

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -5,6 +5,7 @@ import model.GameData;
 import model.UserData;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MySqlDataAccess {
@@ -27,6 +28,31 @@ public class MySqlDataAccess {
             }
         } catch (SQLException e) {
             throw new DataAccessException("unable to clear database", e);
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        String sql = """
+               SELECT username, password, email
+               FROM user
+               WHERE username = ?
+               """;
+        try (var con = DatabaseManager.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, username);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email")
+                    );
+                }
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to get user", e);
         }
     }
 }
