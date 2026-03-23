cf45e2d7f19be683be2eb1b1348fab098bce9f9e
Logan Richardson <logabrichardson13@gmail.com>
2026-03-23 17:15:31 -0600

    creating flags

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -2,9 +2,18 @@ package client;
 
 import model.GameData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Repl {
     private final Scanner scanner = new Scanner(System.in);
     private final ServerFacade server;
+
+    private boolean loggedIn = false;
+    private String authToken = null;
+    private String username = null;
+    private List<GameData> lastListedGames = new ArrayList<>();
+
+
 }
