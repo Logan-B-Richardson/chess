8e2571d8de1161418589d17f55b02daf85f3af0d
Logan Richardson <logabrichardson13@gmail.com>
2026-03-23 17:20:42 -0600

    begin imp of Repl

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -10,10 +10,30 @@ public class Repl {
     private final Scanner scanner = new Scanner(System.in);
     private final ServerFacade server;
 
+    // internal flags
     private boolean loggedIn = false;
     private String authToken = null;
     private String username = null;
     private List<GameData> lastListedGames = new ArrayList<>();
 
-
+    public void run() {
+        System.out.println("Welcome to Chess. Type help to get started.");
+        while (true) {
+            try {
+                if (!loggedIn) {
+                    System.out.print("[LOGGED_OUT] >>> ");
+                    String input = scanner.nextLine().trim();
+                    if (handlePrelogin(input)) {
+                        break;
+                    }
+                } else {
+                    System.out.print("[LOGGED_OUT] >>> ");
+                    String input = scanner.nextLine().trim();
+                    handlePostlogin(input);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
