8a1f27ebd31d9d1ce2504be706dd99b8252d500c
Logan Richardson <logabrichardson13@gmail.com>
2026-03-23 17:14:19 -0600

    building classes

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,10 @@
+package client;
+
+import model.GameData;
+
+import java.util.Scanner;
+
+public class Repl {
+    private final Scanner scanner = new Scanner(System.in);
+    private final ServerFacade server;
+}
