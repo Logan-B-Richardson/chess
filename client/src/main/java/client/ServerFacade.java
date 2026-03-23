fa1af8cd84914f45773eb4bc78247772c626c46a
Logan Richardson <logabrichardson13@gmail.com>
2026-03-23 17:32:32 -0600

    server facade imp

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,14 @@
 package client;
 
+import com.google.gson.Gson;
+
 public class ServerFacade {
+    private final String serverUrl;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+
 }
