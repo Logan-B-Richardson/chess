0e24e9e5ddc59df2352a9acb90f25a8bac06b705
Logan Richardson <logabrichardson13@gmail.com>
2026-03-23 21:45:00 -0600

    working

--- client/src/main/java/ui/BoardUI.java
+++ client/src/main/java/ui/BoardUI.java
@@ -1,4 +1,25 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessBoard;
+
 public class BoardUI {
+    public static void drawBoard(ChessGame game, ChessGame.TeamColor perspective) {
+        if (game == null) {
+            System.out.println("No game to display.");
+            return;
+        }
+        ChessBoard board = game.getBoard();
+        System.out.print(EscapeSequences.ERASE_SCREEN);
+        if (perspective == ChessGame.TeamColor.BLACK) {
+            drawBlackView(board);
+        } else {
+            drawWhiteView(board);
+        }
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+        System.out.println();
+    }
+
 }
