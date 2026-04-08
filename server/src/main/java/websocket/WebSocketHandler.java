940f18f2c50cb821a401eabeb09affbde804dce4
Logan Richardson <logabrichardson13@gmail.com>
2026-04-07 18:37:57 -0600

    class added

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,25 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class WebSocketHandler {
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        System.out.println("Connected");
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) {
+        System.out.println("Received: " + message);
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        System.out.println("Disconnected");
+    }
+}
